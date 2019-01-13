edc38b45b3929c5e9dda131a72b47e9d8dc54b00
Samuel Pearce <samuel.pearce@open.ac.uk>
2019-01-13 17:40:43 +0000

    Adds PublishQuestionType

---
+++ src/Form/PublishQuestionType.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Doctrine\ORM\EntityManagerInterface;
+
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+
+class PublishQuestionType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('Number', HiddenType::class)
+            ->add('publishDate', DateTimeType::class, array(
+                'data' => new \DateTime()
+            ))
+            ->add('scheduleSubsequentQuestions', CheckboxType::class, array(
+                'mapped' => false,
+                'data' => false,
+                'required' => false,
+            ));
+    }
+
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Wec\QuestionsBundle\Entity\Question'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'wec_questionsbundle_question';
+    }
+
+}
