8aabdd9d38603e9f828dc08e045fa4533b1a8c14
Samuel Pearce <samuelcpearce@gmail.com>
2019-11-02 23:26:05 +0000

    #12 Fixes depreciation notices

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,11 +3,11 @@
 namespace App\Controller;
 
 use App\Form\UserLoginType;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
-class SecurityController extends Controller
+class SecurityController extends AbstractController
 {
 
     /**
