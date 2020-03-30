82b4926a9cdb57400ff960cf31e33be60106afb1
Samuel Pearce <samuelcpearce@gmail.com>
2020-03-30 23:29:20 +0100

    Adds Covid community and show feature to main menu

--- src/Controller/WelcomeController.php
+++ src/Controller/WelcomeController.php
@@ -19,27 +19,38 @@ class WelcomeController extends AbstractController
                 "Sermons" => [
                     "path" => $this->generateUrl("sermons_home"),
                     "img" => "build/images/search.svg",
-                    "alt" => ""
+                    "alt" => "",
+                    "show" => true
+                ],
+                "Covid Community" => [
+                    "path" => "https://www.wecmk.org/covid-community/",
+                    "img" => "build/images/team.svg",
+                    "alt" => "",
+                    "show" => true
                 ],
                 "Team Lists" => [
                     "path" => $this->generateUrl("teams_index"),
                     "img" => "build/images/rota.svg",
-                    "alt" => ""
+                    "alt" => "",
+                    "show" => true,
                 ],
                 "Question and Answers" => [
                     "path" => $this->generateUrl('questions_home'),
                     "img" => "build/images/question.svg",
                     "alt" => "",
+                    "show" => true,
                 ],
                 "Logout" => [
                     "path" => $this->generateUrl("security_logout"),
                     "img" => "build/images/logout.svg",
                     "alt" => "",
+                    "show" => true,
                 ],
                 "Public Website" => [
                     "path" => $this->generateUrl("wecmk_public_home"),
                     "img" => "build/images/leftarrow.svg",
                     "alt" => "",
+                    "show" => true,
                 ],
             ],
         );
