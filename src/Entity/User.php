ed7fa5b86bfabe30289e0a0a4ff03765c900cf1d
Samuel Pearce <samuelcpearce@gmail.com>
2021-08-08 01:30:42 +0100

    Update to use new SecurityAuthManager

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -8,6 +8,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
+ * @method string getUserIdentifier()
  */
 class User implements UserInterface
 {
@@ -122,5 +123,10 @@ class User implements UserInterface
         $this->email = $email;
 
         return $this;
+    }
+
+    public function __call($name, $arguments)
+    {
+        // TODO: Implement @method string getUserIdentifier()
     }
 }
