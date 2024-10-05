9a5d2c308fb3b80ff8589a36b346568f409360eb
Samuel Pearce <samuelcpearce@gmail.com>
2024-10-06 00:21:58 +0100

    Adds bootstrap support for asset mapper

--- assets/app.js
+++ assets/app.js
@@ -1,8 +1,10 @@
 import './bootstrap.js';
+import './bootstrap'
 /*
  * Welcome to your app's main JavaScript file!
  *
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/app.css';
--- importmap.php
+++ importmap.php
@@ -16,13 +16,23 @@ return [
         'path' => './assets/app.js',
         'entrypoint' => true,
     ],
-    '@hotwired/stimulus' => [
-        'version' => '3.2.2',
-    ],
     '@symfony/stimulus-bundle' => [
         'path' => './vendor/symfony/stimulus-bundle/assets/dist/loader.js',
     ],
+    '@hotwired/stimulus' => [
+        'version' => '3.2.2',
+    ],
     '@hotwired/turbo' => [
-        'version' => '7.3.0',
+        'version' => '8.0.10',
+    ],
+    '@popperjs/core' => [
+        'version' => '2.11.8',
+    ],
+    'bootstrap/dist/css/bootstrap.min.css' => [
+        'version' => '5.3.3',
+        'type' => 'css',
+    ],
+    'bootstrap' => [
+        'version' => '5.3.3',
     ],
 ];
